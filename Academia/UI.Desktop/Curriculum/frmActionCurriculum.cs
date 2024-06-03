fe6f23c3488c2a674460a455fc6a27a4c7a3d8bd
Agustín Stringa <stringaagustin1@gmail.com>
2024-06-03 20:44:55 -0300

    feat: good aproach Curriculums crud

--- Academia/UI.Desktop/Curriculum/frmActionCurriculum.cs
+++ Academia/UI.Desktop/Curriculum/frmActionCurriculum.cs
@@ -13,23 +13,65 @@ namespace UI.Desktop.Curriculum
 {
     public partial class frmActionCurriculum : Form
     {
+        #region fields
+        private Entities.Curriculum currentCurriculum;
+        private Mode mode;
+        #endregion
+
         #region constructors
         public frmActionCurriculum()
         {
             InitializeComponent();
+            LoadAreas();
         }
 
         public frmActionCurriculum(Mode mode, Entities.Curriculum curr)
         {
             InitializeComponent();
+            currentCurriculum = curr;
+            LoadAreas();
+            if (mode == Mode.Edit)
+            {
+                btnActionCurriculum.Text = "Guardar Plan de Estudios";
+                txtCurriculumId.Visible = true;
+                txtCurriculumId.Text = currentCurriculum.Id.ToString();
+                lblCurriculumId.Visible = true;
+
+                txtCurriculumDescription.Text = currentCurriculum.Description;
+                
+                txtCurriculumYear.Text = currentCurriculum.Year.ToString();
+
+                txtCurriculumResolution.Text = currentCurriculum.Resolution.ToString().Trim();
+
+                //selected value esta bindeado a una lista de areas
+                cbAreas.SelectedValue = currentCurriculum.Area.IdArea;
+
+            }
         }
 
         public frmActionCurriculum(Mode mode)
         {
             InitializeComponent();
+            LoadAreas();
+            this.mode = mode;
+            if (mode == Mode.Create) {
+                btnActionCurriculum.Text = "Crear Plan de Estudios";
+
+                txtCurriculumId.Visible = false;
+                lblCurriculumId.Visible = false;
+            }
         }
         #endregion
 
+
+        private async void LoadAreas()
+        {
+            var areas = await Business.Area.FindAll();
+            cbAreas.DataSource = areas;
+            cbAreas.ValueMember = "IdArea";
+            cbAreas.DisplayMember = "Description";
+            cbAreas.SelectedIndex = 0;
+        }
         private void label3_Click(object sender, EventArgs e)
         {
 
@@ -45,15 +87,81 @@ namespace UI.Desktop.Curriculum
 
         }
 
-        private void btnActionCurriculum_Click(object sender, EventArgs e)
+        private async void btnActionCurriculum_Click(object sender, EventArgs e)
         {
             string description = txtCurriculumDescription.Text;
-            int year = Int32.Parse(txtCurriculumYear.Text);
+            int year = 0;
             string resolution = txtCurriculumResolution.Text;
-            int area = cbAreas.SelectedIndex;
+            try
+            {
+                lblCurriculumYearError.Visible = false;
+                year = Int32.Parse(txtCurriculumYear.Text);
+            }
+            catch (Exception ex)
+            { 
+                lblCurriculumYearError.Visible = true;
+            }
 
-            Entities.Curriculum newCurr = new  Entities.Curriculum(description, area,year, resolution );
-            
+            if (String.IsNullOrEmpty(description))
+            {
+                lblCurriculumDescriptionError.Visible = true;
+            }
+            else
+            {
+                lblCurriculumDescriptionError.Visible = false;
+            }
+
+            if (cbAreas.SelectedValue != null)
+            {
+                int idArea = (int)cbAreas.SelectedValue;
+
+                if (mode == Mode.Create)
+                {
+                    Entities.Curriculum newCurr = new Entities.Curriculum(description, idArea, year, resolution);
+
+                    var curr = await Business.Curriculum.Create(newCurr);
+
+                    if (curr != null)
+                    {
+                        MessageBox.Show(curr.Description + " creado correctamente");
+                        this.Dispose();
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("ERROR");
+                    }
+                }
+                else if (mode == Mode.Edit)
+                {
+                    var area = cbAreas.SelectedItem as Entities.Area;
+                    
+                    var updatedCurriculum = new Entities.Curriculum(
+                        currentCurriculum.Id,
+                        description,
+                        area,
+                        year,
+                        resolution
+                        );
+                    //llamada a update negocio
+                    var update = await Business.Curriculum.Update(updatedCurriculum);
+                    if (update != null)
+                    {
+                        //actualizo campos o cierro form
+                        MessageBox.Show("Actualizado correctamente");
+                    }
+                    else { 
+                        MessageBox.Show("Error al actualizar");
+
+                    }
+                }
+
+                this.Dispose();
+
+            }
+
+
+
         }
     }
 }
