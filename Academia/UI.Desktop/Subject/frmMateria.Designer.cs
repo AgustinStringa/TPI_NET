04fe37bd029d7a7706bf10c1c47941ead840cbc3
Agustín Stringa <stringaagustin1@gmail.com>
2024-08-05 17:22:04 -0300

    chore: initial form inscripcion a materia

--- Academia/UI.Desktop/Subject/frmMateria.Designer.cs
+++ Academia/UI.Desktop/Subject/frmMateria.Designer.cs
@@ -45,7 +45,7 @@
             toolStrip1.Items.AddRange(new ToolStripItem[] { tsbtnAdd, toolStripButton2, toolStripButton3 });
             toolStrip1.Location = new Point(0, 0);
             toolStrip1.Name = "toolStrip1";
-            toolStrip1.Size = new Size(950, 27);
+            toolStrip1.Size = new Size(831, 27);
             toolStrip1.TabIndex = 11;
             toolStrip1.Text = "toolStrip1";
             // 
@@ -55,9 +55,9 @@
             tsbtnAdd.Image = Properties.Resources.Add;
             tsbtnAdd.ImageTransparentColor = Color.Magenta;
             tsbtnAdd.Name = "tsbtnAdd";
-            tsbtnAdd.Size = new Size(29, 24);
+            tsbtnAdd.Size = new Size(24, 24);
             tsbtnAdd.Text = "add";
-            tsbtnAdd.Click += this.tsbtnAdd_Click;
+            tsbtnAdd.Click += tsbtnAdd_Click;
             // 
             // toolStripButton2
             // 
@@ -65,7 +65,7 @@
             toolStripButton2.Image = Properties.Resources.Edit;
             toolStripButton2.ImageTransparentColor = Color.Magenta;
             toolStripButton2.Name = "toolStripButton2";
-            toolStripButton2.Size = new Size(29, 24);
+            toolStripButton2.Size = new Size(24, 24);
             toolStripButton2.Text = "edit";
             // 
             // toolStripButton3
@@ -74,45 +74,48 @@
             toolStripButton3.Image = Properties.Resources.Delete;
             toolStripButton3.ImageTransparentColor = Color.Magenta;
             toolStripButton3.Name = "toolStripButton3";
-            toolStripButton3.Size = new Size(29, 24);
+            toolStripButton3.Size = new Size(24, 24);
             toolStripButton3.Text = "delete";
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(88, 104);
+            dataGridView1.Location = new Point(77, 78);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(751, 279);
+            dataGridView1.Size = new Size(657, 209);
             dataGridView1.TabIndex = 12;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(88, 66);
+            label1.Location = new Point(77, 50);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(38, 15);
             label1.TabIndex = 13;
             label1.Text = "label1";
             // 
             // button1
             // 
-            button1.Location = new Point(745, 57);
+            button1.Location = new Point(652, 43);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(82, 22);
             button1.TabIndex = 14;
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // frmMateria
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(950, 474);
+            ClientSize = new Size(831, 387);
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(dataGridView1);
             Controls.Add(toolStrip1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "frmMateria";
             Text = "frmMateria";
             toolStrip1.ResumeLayout(false);
