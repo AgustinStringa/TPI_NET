b18ec44f0c97d63eba773a93164de826a5060209
Agustín Stringa <stringaagustin1@gmail.com>
2024-08-09 13:47:15 -0300

    feat: CurriculumController initial methods implemented

--- Academia/API/Controllers/CurriculumController.cs
+++ Academia/API/Controllers/CurriculumController.cs
@@ -7,13 +7,123 @@ namespace API.Controllers
     [Route("api/curriculums")]
 
     [ApiController]
-    public class CurriculumController
+    public class CurriculumController : Controller
     {
         [HttpGet]
-        public List<Curriculum> GetCurriculums()
+        public async Task<ActionResult<IEnumerable<Curriculum>>> GetAll()
         {
-            var context = new AcademiaContext();
-            return context.Curriculums.Include(c => c.Area).ToList();
+            try
+            {
+                var context = new AcademiaContext();
+                return await context.Curriculums.Include(c => c.Area).ToListAsync();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = e.Message });
+                throw e;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Curriculum>> GetById(int id)
+        {
+            try
+            {
+                var context = new AcademiaContext();
+                var curriculum = await context.Curriculums.FindAsync(id);
+                if (curriculum == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    await context.Entry(curriculum).Reference(c => c.Area).LoadAsync();
+                    return curriculum;
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = e.Message });
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Curriculum>> Create(Curriculum newCurriculum)
+        {
+            try
+            {
+                var context = new AcademiaContext();
+                context.Curriculums.Add(newCurriculum);
+                await context.SaveChangesAsync();
+                return CreatedAtAction(
+                nameof(GetById),
+                new { id = newCurriculum.Id }, newCurriculum);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = e.Message });
+                throw e;
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Curriculum updatedCurriculum)
+        {
+            try
+            {
+                if (id != updatedCurriculum.Id)
+                {
+                    return BadRequest();
+                }
+                var context = new AcademiaContext();
+                var curriculum = await context.Curriculums.FindAsync(id);
+                if (curriculum == null)
+                {
+                    return NotFound();
+                }
+                //prop by prop        
+                curriculum.Description = updatedCurriculum.Description;
+                curriculum.Year = updatedCurriculum.Year;
+                curriculum.Resolution = updatedCurriculum.Resolution;
+                var area = await context.Areas.FindAsync(updatedCurriculum.AreaId);
+                if (area != null)
+                {
+                    curriculum.AreaId = updatedCurriculum.AreaId;
+                    curriculum.Area = area;
+                }
+                await context.SaveChangesAsync();
+                return NoContent();
+                //return Ok(curriculum);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = e.Message });
+                throw e;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var context = new AcademiaContext();
+                var deletedCurriculum = await context.Curriculums.FindAsync(id);
+                if (deletedCurriculum == null)
+                {
+                    return NotFound();
+                }
+                context.Curriculums.Remove(deletedCurriculum);
+                await context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = e.Message });
+                throw e;
+            }
         }
     }
 }
