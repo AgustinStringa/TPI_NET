1f1b9f240633ca0ff3349d6c4023db4651609b20
Agustín Stringa <stringaagustin1@gmail.com>
2024-08-12 19:07:42 -0300

    chore: update userService
    validetecredentials return related entities.

--- Academia/Domain/Services/UserService.cs
+++ Academia/Domain/Services/UserService.cs
@@ -11,12 +11,21 @@ namespace Domain.Services
 {
     public class UserService : IUserService
     {
-        
+
         public void Add(User user)
         {
-            var context = new AcademiaContext();
-            context.Users.Add(user);
-            context.SaveChanges();
+            try
+            {
+                var context = new AcademiaContext();
+                context.Users.Add(user);
+                context.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+               
+                throw;
+            }
         }
 
         public void Delete()
@@ -40,27 +49,44 @@ namespace Domain.Services
             throw new NotImplementedException();
         }
 
-        public User ValidateCredentials(string username, string password)
+        public async Task<User> ValidateCredentials(string username, string password)
         {
-
-            var context = new AcademiaContext();
-            Domain.Model.User user = context.Users.FirstOrDefault(u => u.Username == username);
-            if (user != null) { 
-                byte[] sentHashValue = Convert.FromHexString(user.Password);
-                byte[] messageBytes1 = Encoding.UTF8.GetBytes(password);
-                byte[] compareHashValue = SHA256.HashData(messageBytes1);
-                if (sentHashValue.SequenceEqual(compareHashValue))
+            try
+            {
+                var context = new AcademiaContext();
+                Domain.Model.User user = context.Users.FirstOrDefault(u => u.Username == username);
+                if (user != null)
                 {
-                    return user;
+                    byte[] sentHashValue = Convert.FromHexString(user.Password);
+                    byte[] messageBytes1 = Encoding.UTF8.GetBytes(password);
+                    byte[] compareHashValue = SHA256.HashData(messageBytes1);
+                    if (sentHashValue.SequenceEqual(compareHashValue))
+                    {
+                        await context.Entry(user).Reference(c => c.Curriculum).LoadAsync();
+                        await context.Entry(user).Collection(c => c.UserCourses).LoadAsync();
+                        foreach (var uc in user.UserCourses)
+                        {
+                            await context.Entry(uc).Reference(c => c.Course).LoadAsync();
+                            await context.Entry(uc.Course).Reference(c => c.Subject).LoadAsync();
+                        }
+                        return user;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+
                 }
-                else {
+                else
+                {
                     return null;
                 }
 
             }
-            else
+            catch (Exception ex)
             {
-                return null;
+                Console.WriteLine(ex.Message);
+                throw;
             }
 
 
