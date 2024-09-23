17e5980939024072840adb8640da444651181823
eugen <eugen@DESKTOP-L1E8H85>
2024-09-23 17:04:11 -0300

    fix: AcademiaContext connection string

--- Academia/API/AcademiaContext.cs
+++ Academia/API/AcademiaContext.cs
@@ -11,25 +11,17 @@ namespace API
         public DbSet<Course> Courses { get; set; } = null!;
         public DbSet<Curriculum> Curriculums { get; set; } = null!;
         public DbSet<User> Users { get; set; } = null!;
-<<<<<<< HEAD
         public DbSet<Commission> Commissions { get; set; } = null!;
-=======
         public DbSet<UserCourse> UserCourses { get; set; } = null!;
->>>>>>> d6204c7f98f08b2ef5d0ebde6794592f57b801e0
 
         private string _connectionString = "";
         public AcademiaContext()
         {
-<<<<<<< HEAD
-            _connectionString = "Data Source=DESKTOP-L1E8H85\\SQLEXPRESS;Initial Catalog=academia;Integrated Security=True;TrustServerCertificate=True\r\n";
-=======
-
             var configuration = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
           .AddJsonFile("appsettings.json")
           .Build();
             _connectionString = configuration.GetConnectionString("AcademiaDevConnectionString");         
->>>>>>> d6204c7f98f08b2ef5d0ebde6794592f57b801e0
             this.Database.EnsureCreated();
         }
 
