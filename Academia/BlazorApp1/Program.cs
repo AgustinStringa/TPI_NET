6177b1628037d1ec9ad6f189d503b713d0d84c85
Agustín Stringa <stringaagustin1@gmail.com>
2024-10-08 00:36:46 -0300

    feat: Blazor Project created / inital Area Page created

---
+++ Academia/BlazorApp1/Program.cs
@@ -0,0 +1,40 @@
+using BlazorApp1.Components;
+using ClientService.Area;
+
+namespace BlazorApp1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
+			builder.Services.AddBlazorBootstrap();
+			builder.Services.AddHttpClient();
+			builder.Services.AddScoped<IAreaService, AreaService>();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+			app.UseAntiforgery();
+
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
+
+			app.Run();
+		}
+	}
+}
